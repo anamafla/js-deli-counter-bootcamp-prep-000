685303ea269570038b0e897f4e8c576e5db890a7
Ana Mafla <anamafla@hotmail.com>
2018-08-11 14:14:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(currentLine, name){
+  currentLine.push(name);
+  let position = currentLine.length;
+  return (`Welcome, ${name}. You are number ${position} in line.`)
+}
+
+function nowServing(currentLine){
+  if (currentLine.length > 0 )
+   return(`Currently serving ${currentLine.shift()}.`)
+  else
+   return ("There is nobody waiting to be served!");
+}
+
+function currentLine(currentLine){
+  let newCurrentLine = "The line is currently: ";
+  if (currentLine.length > 0){
+    for (let i = 1; i < currentLine.length; i ++){
+     newCurrentLine = newCurrentLine.concat(`${i}. ${currentLine[i]}`); 
+    }
+    return newCurrentLine;
+  } else {
+    return ("The line is currently empty.")
+  }
+  
+}
